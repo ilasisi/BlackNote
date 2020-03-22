41402beefc84de1fdae3e2bbed263e84350291a6
Lasisi Ibrahim Gbenga <ilasisi90@gmail.com>
2020-03-22 15:56:32 +0100

    Initial commit

---
+++ app/src/main/java/com/black/note/org/add_note/AddNoteViewModel.kt
@@ -0,0 +1,7 @@
+package com.black.note.org.note
+
+import androidx.lifecycle.ViewModel
+
+class NoteViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/black/note/org/edit_note/EditNoteViewModel.kt
@@ -0,0 +1,7 @@
+package com.black.note.org.edit_note
+
+import androidx.lifecycle.ViewModel
+
+class EditNoteViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
